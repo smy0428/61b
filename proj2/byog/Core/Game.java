2dd073199dd2014c36cf5aa68f8ce8ef72ee0169
时思芫 <SteeleShi@shisiyuandeMBP.fritz.box>
2020-03-21 10:09:18 +0100

    to share with LIN

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -2,17 +2,26 @@ package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
-    TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
-    public static final int WIDTH = 80;
+    public static final int WIDTH = 30;
     public static final int HEIGHT = 30;
+    private static final long SEED = 28042007;
+    private static final Random RANDOM = new Random(SEED);
+    Position start;
+
+
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
     public void playWithKeyboard() {
+        return;
     }
 
     /**
@@ -28,11 +37,210 @@ public class Game {
      * @return the 2D TETile[][] representing the state of the world
      */
     public TETile[][] playWithInputString(String input) {
-        // TODO: Fill out this method to run the game using the input passed in,
+        // Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
 
-        TETile[][] finalWorldFrame = null;
+        // TETile[][] finalWorldFrame = null;
+
+        /** first generate a empty world. */
+        TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                finalWorldFrame[x][y] = Tileset.NOTHING;
+            }
+        }
         return finalWorldFrame;
+    }
+
+
+    /**
+     * simple class to present the position with x and y
+     */
+    public static class Position {
+        private int x;
+        private int y;
+        public Position(int pX, int pY) {
+            x = pX;
+            y = pY;
+        }
+    }
+
+
+    /**
+     * not so simple class to present the world map of exterior/wall/floor
+     */
+    public static class Worldmap {
+        private int floorTiles;
+        private int wallTiles;
+        private String[][] propertyMap;
+
+        public Worldmap() {
+            propertyMap = new String[WIDTH][HEIGHT];
+            for (int i = 0; i < WIDTH; i += 1) {
+                for (int k = 0; k < HEIGHT; k += 1) {
+                    propertyMap[i][k] = "exterior";
+                }
+            }
+        }
+
+
+        public void addWall(Position p) {
+            if (propertyMap[p.x][p.y].equals("exterior")) {
+                propertyMap[p.x][p.y] = "wall";
+                wallTiles += 1;
+            }
+        }
+
+        public void addFloor(Position p) {
+            if (propertyMap[p.x][p.y].equals("exterior")) {
+                propertyMap[p.x][p.y] = "floor";
+                floorTiles += 1;
+            }
+            if (propertyMap[p.x][p.y].equals("wall")) {
+                propertyMap[p.x][p.y] = "floor";
+                floorTiles += 1;
+                wallTiles -= 1;
+            }
+        }
+
+        public double usageRatio(){
+            double usage = 1.0 * (floorTiles + wallTiles) / (WIDTH * HEIGHT);
+            return usage;
+        }
+    }
+
+
+
+    public static class Room {
+        private Position randomP;
+        private Position[] floorTiles;
+        private Position[] wallTiles;
+        // use width, height and left bottom corner to generate a square
+        public void drawRoom (int w, int h, Position p) {
+            wallTiles = new Position[2 * (w + h - 2)];
+            floorTiles = new Position[(w - 2) * (h - 2)];
+
+            // wall bottom line
+            for (int i = 0; i < w; i += 1) {
+                wallTiles[i] = new Position(p.x + i, p.y);
+            }
+            // wall top line
+            for (int i = 0; i < w; i += 1) {
+                wallTiles[w + i] = new Position(p.x + i, p.y + h - 1);
+            }
+            // wall left line
+            for (int i = 0; i < h - 2; i += 1) {
+                wallTiles[2 * w + i] = new Position(p.x, p.y + i + 1);
+            }
+            // wall right line
+            for (int i = 0; i < h - 2; i += 1) {
+                wallTiles[2 * w + h - 2 + i] = new Position(p.x + w - 1, p.y + i + 1);
+            }
+
+            // floor square
+            int m = 0;
+            for (int i = 1; i < w - 1; i += 1) {
+                for (int k = 1; k < h - 1; k += 1) {
+                    floorTiles[m] = new Position(p.x + i, p.y + k);
+                    m += 1;
+                }
+            }
+            int i = RandomUtils.uniform(RANDOM, 0, floorTiles.length);
+            randomP = floorTiles[i];
+        }
+    }
+
+
+
+    public void addFirstSquare(Room first, Worldmap m) {
+        for (Position pp: first.wallTiles) {
+            m.addWall(pp);
+        }
+        for (Position pp: first.floorTiles) {
+            m.addFloor(pp);
+        }
+    }
+
+
+    public void addNextSquare(int width, int height, Room pre, Worldmap m) {
+        Room next = new Room();
+        next.drawRoom(width, height, pre.randomP);
+        for (Position pp: next.wallTiles) {
+            m.addWall(pp);
+        }
+        for (Position pp: next.floorTiles) {
+            m.addFloor(pp);
+        }
+    }
+
+
+    public void addNextNSquare(Room pre, Worldmap m) {
+        while (m.usageRatio() < 0.5) {
+            //(x, y) random point in the screen
+            int x = RANDOM.nextInt(WIDTH);
+            while (Math.abs(pre.randomP.x - x) < 3 ) {
+                x = RANDOM.nextInt(WIDTH);
+            }
+            int y = RANDOM.nextInt(HEIGHT);
+            while (Math.abs(pre.randomP.y - y) < 3) {
+                y = RANDOM.nextInt(HEIGHT);
+            }
+
+            // get the right width, height, start point of new room
+            pre.randomP = new Position(Math.min(pre.randomP.x, x), Math.min(pre.randomP.y, y));
+            int width = Math.abs(pre.randomP.x - x);
+            int height = Math.abs(pre.randomP.y - y);
+
+            Room next = new Room();
+            next.drawRoom(width, height, pre.randomP);
+            for (Position pp : next.wallTiles) {
+                m.addWall(pp);
+            }
+            for (Position pp : next.floorTiles) {
+                m.addFloor(pp);
+            }
+        }
+    }
+
+
+
+
+
+    /**
+     * quick test for the code
+     */
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                finalWorldFrame[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        Position start = new Position(0, 0);
+        Worldmap m = new Worldmap();
+        Game g = new Game();
+        Room first = new Room();
+        first.drawRoom(4, 4, start);
+        g.addFirstSquare(first, m);
+        g.addNextNSquare(first, m);
+
+
+        for (int i = 0; i < m.propertyMap.length; i += 1) {
+            for (int k = 0; k < m.propertyMap[i].length; k += 1) {
+                if (m.propertyMap[i][k].equals("floor")) {
+                    finalWorldFrame[i][k] = Tileset.MOUNTAIN;
+                }
+                if (m.propertyMap[i][k].equals("wall")) {
+                    finalWorldFrame[i][k] = Tileset.WALL;
+                }
+            }
+        }
+
+        ter.renderFrame(finalWorldFrame);
     }
 }
--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -6,10 +6,118 @@ import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import java.awt.*;
+import java.util.*;
+
+import java.util.List;
 import java.util.Random;
 
 /**
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 40;
+    private static final int HEIGHT = 40;
+
+
+    //** simple class to present the lower left corner of the hexagon */
+    public static class Position {
+        public int x;
+        public int y;
+        public Position(int pX, int pY) {
+            x = pX;
+            y = pY;
+        }
+    }
+
+
+    public static Position rightDown(Position p, int len) {
+        Position q = new Position(p.x + 2 * len - 1, p.y - len);
+        return q;
+    }
+
+
+    public static Position rightUp(Position p, int len) {
+        Position q = new Position(p.x + 2 * len - 1, p.y + len);
+        return q;
+    }
+
+
+    public static int start(int h, int l) {
+        int w = l - h - 1;
+        if (w >= 0) {
+            return w;
+        }
+        return -(1 + w);
+    }
+
+    public static int end(int h, int l) {
+        int w = 3 * l - 2 - start(h, l);
+        return w;
+    }
+
+
+    //** draw a simple hexagon */
+    public static void addHexagon(TETile[][] world, int length, Position p, TETile t) {
+        List<Position> drawPosition = new ArrayList<Position>();
+
+        //** get the right positions to draw the hexagon */
+        for (int h = 0; h < 2 * length; h += 1) {
+            for (int w = start(h, length); w < end(h, length); w += 1) {
+                Position q = new Position(w + p.x, h + p.y);
+                drawPosition.add(q);
+            }
+        }
+
+        for (Position pp : drawPosition) {
+            world[pp.x][pp.y] = t;
+        }
+    }
+
+
+    public static void drawRandomVerticalHexes(TETile[][] world, int length, int num, Position p, TETile t) {
+        for (int i = 0; i < num; i += 1) {
+            addHexagon(world, length, p, t);
+            p = new Position(p.x, p.y + 2 * length);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] hexagonTiles = new TETile[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x += 1) {
+            for (int y = 0; y < HEIGHT; y += 1) {
+                hexagonTiles[x][y] = Tileset.NOTHING;
+            }
+        }
+
+        Position start1 = new Position(0, 10);
+        TETile t1 = new TETile('@', Color.white, Color.black, "@");
+        drawRandomVerticalHexes(hexagonTiles, 3, 3, start1, t1);
+
+        Position start2 = rightDown(start1, 3);
+        TETile t2 = new TETile('%', Color.blue, Color.black, "%");
+        drawRandomVerticalHexes(hexagonTiles, 3, 4, start2, t2);
+
+
+        Position start3 = rightDown(start2, 3);
+        TETile t3 = new TETile('&', Color.red, Color.black, "%");
+        drawRandomVerticalHexes(hexagonTiles, 3, 5, start3, t3);
+
+
+        Position start4 = rightUp(start3, 3);
+        TETile t4 = new TETile('#', Color.green, Color.black, "%");
+        drawRandomVerticalHexes(hexagonTiles, 3, 4, start4, t4);
+
+        Position start5 = rightUp(start4, 3);
+        TETile t5 = new TETile('*', Color.yellow, Color.black, "%");
+        drawRandomVerticalHexes(hexagonTiles, 3, 3, start5, t5);
+
+
+        // addHexagon(hexagonTiles, 8, start, t);
+        ter.renderFrame(hexagonTiles);
+    }
 }
