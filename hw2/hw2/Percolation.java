94c1d3ccba1e34eda0800c3dc25192b541e639a6
时思芫 <SteeleShi@shisiyuandeMBP.fritz.box>
2020-04-01 18:10:27 +0200

    double union

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,6 +4,7 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     private WeightedQuickUnionUF unionGrid;
+    private WeightedQuickUnionUF unionGridNoBottom;
     private int[][] openingGrid;
     private int openings;
     private int size;
@@ -16,12 +17,9 @@ public class Percolation {
             size = N;
             openings = 0;
             unionGrid = new WeightedQuickUnionUF(N * N + 2);
+            unionGridNoBottom = new WeightedQuickUnionUF(N * N + 1);
             openingGrid = new int[N][N];
             for (int i = 0; i < N; i += 1) {
-                // connect the 0th row to the top point
-                unionGrid.union(i, N * N);
-                // connect the (N-1)th row to the bottom point
-                //unionGrid.union(i + (N - 1) * N, N * N + 1);
                 for (int j = 0; j < N; j += 1) {
                     openingGrid[i][j] = 0;
                 }
@@ -38,8 +36,12 @@ public class Percolation {
                 openingGrid[row][col] = 1;
                 openings += 1;
                 connect(row, col);
-                if (row == size - 1 && isFull(row, col)) {
-                    int id = row * size + col;
+                int id = row * size + col;
+                if (row == 0) {
+                    unionGrid.union(size * size, id);
+                    unionGridNoBottom.union(size * size, id);
+                }
+                if (row == size - 1) {
                     unionGrid.union(size * size + 1, id);
                 }
             }
@@ -63,7 +65,7 @@ public class Percolation {
             return false;
         } else {
             int id = row * size + col;
-            return unionGrid.connected(id, size * size);
+            return unionGridNoBottom.connected(id, size * size);
         }
     }
 
@@ -72,18 +74,22 @@ public class Percolation {
         int id = row * size + col;
         if (row > 0 && isOpen(row - 1, col)) {
             unionGrid.union(id, id - size);
+            unionGridNoBottom.union(id, id - size);
         }
 
         if (row < size - 1 && isOpen(row + 1, col)) {
             unionGrid.union(id, id + size);
+            unionGridNoBottom.union(id, id + size);
         }
 
         if (col > 0 && isOpen(row, col - 1)) {
             unionGrid.union(id, id - 1);
+            unionGridNoBottom.union(id, id - 1);
         }
 
         if (col < size - 1 && isOpen(row, col + 1)) {
             unionGrid.union(id, id + 1);
+            unionGridNoBottom.union(id, id + 1);
         }
     }
 
@@ -96,10 +102,4 @@ public class Percolation {
     public boolean percolates() {
         return unionGrid.connected(size * size, size * size + 1);
     }
-
-
-    public static void main(String[] args) {
-
-    }
-
 }
