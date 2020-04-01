ba2c809a7a36b8cf7d53a080a92373c64b7edc46
Siyuan Sh <shimuyi0428@gmail.com>
2020-04-01 16:36:45 +0200

    interesting

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,103 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF unionGrid;
+    private int[][] openingGrid;
+    private int openings;
+    private int size;
+
+
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        } else {
+            size = N;
+            openings = 0;
+            unionGrid = new WeightedQuickUnionUF(N * N + 2);
+            openingGrid = new int[N][N];
+            for (int i = 0; i < N; i += 1) {
+                // connect the 0th row to the top point
+                unionGrid.union(i, N * N);
+                // connect the (N-1)th row to the bottom point
+                //unionGrid.union(i + (N - 1) * N, N * N + 1);
+                for (int j = 0; j < N; j += 1) {
+                    openingGrid[i][j] = 0;
+                }
+            }
+        }
+    }
+
+
+    public void open(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else {
+            if (!isOpen(row, col)) {
+                openingGrid[row][col] = 1;
+                openings += 1;
+                connect(row, col);
+                if (row == size - 1 && isFull(row, col)) {
+                    int id = row * size + col;
+                    unionGrid.union(size * size + 1, id);
+                }
+            }
+        }
+    }
+
+
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else {
+            return openingGrid[row][col] > 0;
+        }
+    }
+
+
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row >= size || col < 0 || col >= size) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else if (!isOpen(row, col)) {
+            return false;
+        } else {
+            int id = row * size + col;
+            return unionGrid.connected(id, size * size);
+        }
+    }
+
+
+    private void connect(int row, int col) {
+        int id = row * size + col;
+        if (row > 0 && isOpen(row - 1, col)) {
+            unionGrid.union(id, id - size);
+        }
+
+        if (row < size - 1 && isOpen(row + 1, col)) {
+            unionGrid.union(id, id + size);
+        }
+
+        if (col > 0 && isOpen(row, col - 1)) {
+            unionGrid.union(id, id - 1);
+        }
+
+        if (col < size - 1 && isOpen(row, col + 1)) {
+            unionGrid.union(id, id + 1);
+        }
+    }
+
+
+    public int numberOfOpenSites() {
+        return openings;
+    }
+
+
+    public boolean percolates() {
+        return unionGrid.connected(size * size, size * size + 1);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,81 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import edu.princeton.cs.algs4.Stopwatch;
 
+public class PercolationStats {
+    private double[] fractionOpen;
+    private StdRandom RANDOM;
+    private int times;
+
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        } else {
+            fractionOpen = new double[T];
+            times = T;
+            for (int i = 0; i < T; i += 1) {
+                Percolation p = pf.make(N);
+                while (!p.percolates()) {
+                    int id = RANDOM.uniform(N * N);
+                    while (p.isOpen(helperRow(N, id), helperCol(N, id))) {
+                        id = RANDOM.uniform(N * N);
+                    }
+                    p.open(helperRow(N, id), helperCol(N, id));
+                }
+                fractionOpen[i] = p.numberOfOpenSites();
+            }
+        }
+    }
+
+
+    private int helperRow(int N, int id) {
+        return id % N;
+    }
+
+
+    private int helperCol(int N, int id) {
+        return id / N;
+    }
+
+
+    public double mean() {
+        double m = StdStats.mean(fractionOpen);
+        return m;
+    }
+
+
+    public double stddev() {
+        double s = StdStats.stddev(fractionOpen);
+        return s;
+    }
+
+
+    public double confidenceLow() {
+        double cL = StdStats.mean(fractionOpen)
+                - 1.96 * Math.sqrt(StdStats.stddev(fractionOpen) / times);
+        return cL;
+    }
+
+
+    public double confidenceHigh() {
+        double cH = StdStats.mean(fractionOpen)
+                + 1.96 * Math.sqrt(StdStats.stddev(fractionOpen) / times);
+        return cH;
+    }
+
+
+    public static void main(String[] args) {
+        Stopwatch timer1 = new Stopwatch();
+
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats plS = new PercolationStats(160, 100, pf);
+        System.out.println(plS.confidenceLow());
+        System.out.println(plS.confidenceHigh());
+
+        double time1 = timer1.elapsedTime();
+        System.out.println("time used is " + time1);
+    }
 }
